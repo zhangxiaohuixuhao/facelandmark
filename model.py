48529fdcc14fc093817e7e0ce37da86d877bd490
zhangxiaohuixuhao <1215002572.com>
2020-12-20 12:59:55 +0800

    model

--- model.py
+++ model.py
@@ -54,3 +54,34 @@ class model(nn.Module):
         x = self.ip3(x)
         return x
 
+class LeNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1)
+        self.relu1 = nn.ReLU()
+        self.pooling1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.relu2 = nn.ReLU()
+        self.pooling2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1)
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(1*1*120, 84)
+        self.fc2 = nn.Linear(84, num_classes)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pooling1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pooling2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+
+    
+
+
+
